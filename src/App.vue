3b0344fd424bf10cfe8c6bd546d07e088891d9f0
smeemarket <smeemarket@gmail.com>
2022-06-11 12:03:09 +0630

    from ph

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Phone updating..." />
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
